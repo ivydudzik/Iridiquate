12efb9a9cb8d5e860adcb2fe3cd53f5553d6a2c9
Ivy Dudzik <ivydudzik@gmail.com>
2024-04-13 23:30:58 -0700

    Add Iridiquate Locations and Begin Work on Interactables

--- rules.js
+++ rules.js
@@ -27,6 +27,31 @@ class Location extends Scene {
         if (choice) {
             this.engine.show("&gt; " + choice.Text);
             this.engine.gotoScene(Location, choice.Target);
+            if (choice.IsBombLocation == true) {
+                console.log("this is a bomb location!")
+                this.engine.gotoScene(BombLocation, choice.Target);
+            } else {
+                this.engine.gotoScene(Location, choice.Target);
+
+            }
+        } else {
+            this.engine.gotoScene(End);
+        }
+    }
+}
+
+class BombLocation extends Location {
+    handleChoice(choice) {
+        console.log("this is a bomb location handling a choice!")
+        if (choice) {
+            if (choice.Interaction) {
+                this.engine.show("&gt; " + choice.Text);
+                this.engine.storyData.Locations[choice.Target].Body = choice.NewBody;/// MESSY TRYING TO FIGURE OUT HOW COME IT HAPPENS TWICE AND HOW TO CHANGE STORYDATA ON THE FLY
+                this.engine.gotoScene(Location, choice.Target);
+            } else {
+                this.engine.show("&gt; " + choice.Text);
+                this.engine.gotoScene(Location, choice.Target);
+            }
         } else {
             this.engine.gotoScene(End);
         }
@@ -40,4 +65,4 @@ class End extends Scene {
     }
 }
 
-Engine.load(Start, 'myStory.json');
+Engine.load(Start, 'myStory.json'); // Creates Engine Instance and Starts Game
